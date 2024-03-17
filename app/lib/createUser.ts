7112eca51080e6073a7461f475021af697a02df1
Nico <nicosetzes@gmail.com>
2024-03-17 12:08:46 -0300

    V1.4 I'm experimenting with Prisma and server components.

---
+++ app/lib/createUser.ts
@@ -0,0 +1,15 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default async function prismaExample(name, email, image) {
+//   const newUser = await prisma.user.create({
+//     data: {
+//       name,
+//       email,
+//       image,
+//     },
+//   });
+
+//   const users = await prisma.user.findMany();
+// }
